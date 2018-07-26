e22cacde2326ce0d17c07fa0d3aa8f78e77e6aa2
squamuglia <maxsmouha@gmail.com>
2018-07-26 17:48:37 -0400

    max changes

--- src/MonsterContainer.js
+++ src/MonsterContainer.js
@@ -1,17 +1,36 @@
-import {SketchField, Tools} from 'react-sketch';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Two from 'two.js';
 
 class MonsterContainer extends Component {
-     render() {
-        return (
-            <SketchField width='1024px'
-                         height='768px'
-                         tool={Tools.Pencil}
-                         lineColor='black'
-                         lineWidth={3}/>
-        )
-     }
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.line = null;
+    this.two = null;
+  }
+
+  componentWillMount() {
+    let two = new Two({
+      type: Two.Types[this.props.type],
+      fullscreen: true
+    });
+    this.two = new Two();
+  }
+
+  componentDidMount() {
+    const stage = this.refs;
+    console.log(stage);
+    // this.two.appendTo(stage);
+  }
+
+  render() {
+    return (
+      <div>
+        <div ref="stage" style={{ height: 100 + '%' }} />
+      </div>
+    );
+  }
 }
 
-
-export default MonsterContainer
+export default MonsterContainer;
