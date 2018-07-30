1e49ed493e1afb89701d4ed727a0e5579b90e1c8
github username <github email address>
2018-07-30 18:13:35 -0400

    asdfawef a

--- src/containers/MonsterContainer.js
+++ src/containers/MonsterContainer.js
@@ -4,13 +4,30 @@ import { SketchField, Tools } from 'react-sketch';
 class MonsterContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentPart: 'heads'
+    };
   }
+
+  changePart = part => {
+    this.setState(
+      {
+        currentPart: part
+      },
+      () => console.log('partstate', this.state)
+    );
+  };
+
   getImage = e => {
     e.preventDefault();
     const pngURI = document.getElementsByTagName('canvas')[0].toDataURL();
     const data = { user_id: 1, url: pngURI };
-    fetch(`http://localhost:3000/api/v1/heads`, {
+    console.log('data before fetch', data);
+    console.log(
+      'fetch url',
+      `http://localhost:3000/api/v1/${this.state.currentPart}`
+    );
+    fetch(`http://localhost:3000/api/v1/${this.state.currentPart}`, {
       method: 'POST',
 
       headers: {
@@ -20,7 +37,7 @@ class MonsterContainer extends Component {
       body: JSON.stringify(data)
     })
       .then(r => r.json())
-      .then(r => console.log(r));
+      .then(r => console.log('drawing post response', r));
 
     // const form = document.querySelector('#form1');
     // const data = new FormData(form);
@@ -38,17 +55,45 @@ class MonsterContainer extends Component {
 
   render() {
     return (
-      <div className="f jic aic mt1">
-        <div className="fa dashed border sq">
-          <SketchField
-            width="100%"
-            height="100%"
-            tool={Tools.Pencil}
-            lineColor="black"
-            lineWidth={5}
-          />
+      <React.Fragment>
+        <div className="x f">
+          <div className="fa ac">
+            <button
+              className="px2 py05 mt1 h5"
+              onClick={() => this.changePart('heads')}
+            >
+              Head
+            </button>
+          </div>
+          <div className="fa ac">
+            <button
+              className="px2 py05 mt1 h5"
+              onClick={() => this.changePart('hands')}
+            >
+              Body
+            </button>
+          </div>
+          <div className="fa ac">
+            <button
+              className="px2 py05 mt1 h5"
+              onClick={() => this.changePart('feet')}
+            >
+              Feet
+            </button>
+          </div>
+        </div>
 
-          {/* <form id="form1">
+        <div className="f jic aic mt1">
+          <div className="fa dashed border sq">
+            <SketchField
+              width="100%"
+              height="100%"
+              tool={Tools.Pencil}
+              lineColor="black"
+              lineWidth={5}
+            />
+
+            {/* <form id="form1">
             <input type="file" name="image" id="image_upload" accept="image/*" />
             <input
               type="submit"
@@ -58,11 +103,12 @@ class MonsterContainer extends Component {
             />
           </form> */}
 
-          <button className="px2 py05 mt1 h5" onClick={this.getImage}>
-            Submit
-          </button>
+            <button className="px2 py05 mt1 h5" onClick={this.getImage}>
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
